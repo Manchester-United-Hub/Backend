72ddb776d09437f90fe2ce3fcdf640b5d6f50978
kimbc715 <kimbc715@gmail.com>
2026-04-20 19:40:19 +0900

    init: initial project setup with Spring Boot, JPA, and MySQL configuration

---
+++ src/test/java/backend/manuhub/ManuhubApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.manuhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
