f11520b491ad2f3251627260a3caa635ed9502ba
kimbc715 <kimbc715@gmail.com>
2026-04-25 20:43:56 +0900

    feat: add NaverNewsResponse

---
+++ src/main/java/backend/manuhub/external/naver/NaverNewsResponse.java
@@ -0,0 +1,36 @@
+package backend.manuhub.external.naver;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+public class NaverNewsResponse {
+
+    private List<NaverNewsItem> items;
+
+    @Getter
+    @NoArgsConstructor
+    public static class NaverNewsItem {
+
+        @JsonProperty("title")
+        private String title;
+
+        @JsonProperty("originallink")
+        private String originalLink;
+
+        @JsonProperty("link")
+        private String link;
+
+        @JsonProperty("description")
+        private String description;
+
+        @JsonProperty("pubDate")
+        private String publishedAt;
+    }
+}
