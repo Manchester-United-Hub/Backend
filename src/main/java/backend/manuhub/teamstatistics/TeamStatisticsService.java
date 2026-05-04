1e16772557e2fd0da3ee412a97101522d2a9cbf8
asdfzxcv53 <sskij@naver.com>
2026-05-04 16:58:05 +0900

    feat: add TeamStatistics save method by season #7

--- src/main/java/backend/manuhub/teamstatistics/TeamStatisticsService.java
+++ src/main/java/backend/manuhub/teamstatistics/TeamStatisticsService.java
@@ -1,17 +1,12 @@
 package backend.manuhub.teamstatistics;
 
+import backend.manuhub.external.teamstatistics.TeamStatisticsApiResponse;
+import backend.manuhub.external.teamstatistics.TeamStatisticsClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestClientException;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +14,7 @@ import java.util.stream.Collectors;
 public class TeamStatisticsService {
 
     private final TeamStatisticsRepository teamStatisticsRepository;
+    private final TeamStatisticsClient teamStatisticsClient;
 
     @Transactional(readOnly = true)
     public List<TeamStatisticsResponse> getAllTeamStatistics() {
@@ -28,8 +24,27 @@ public class TeamStatisticsService {
         List<TeamStatistics> statisticsList = teamStatisticsRepository.findAllByOrderBySeasonAsc();
 
         return statisticsList.stream()
-                .sorted(Comparator.comparing(TeamStatistics::getSeason))
                 .map(TeamStatisticsResponse::from)
                 .toList();
+    }
+
+    @Transactional
+    public void saveTeamStatistics(Integer season) {
+        log.info("[TeamStatistics] {}년도 기록 저장 시작", season.toString());
+
+        if(teamStatisticsRepository.existsBySeason(season)) {
+            log.warn(">>> TeamStatistics --> EntityAlreadyExistsException. season = {}. TeamStatistics Entity Already Exists",
+                    season);
+            return;
+        }
+
+        TeamStatisticsApiResponse response =
+                teamStatisticsClient.getTeamStatistics(season);
+
+        TeamStatistics teamStatistics = response.toEntity();
+
+        teamStatisticsRepository.save(teamStatistics);
+
+        log.info("[TeamStatistics] {}년도 기록 저장 완료", season.toString());
     }
 }
