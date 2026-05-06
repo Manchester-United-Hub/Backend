3a4f519ea40b08eeb60393ee79fe4686aaa9055e
kimbc715 <kimbc715@gmail.com>
2026-05-06 16:30:38 +0900

    feat: add NewsGetReponse

---
+++ src/main/java/backend/manuhub/news/NewsGetResponse.java
@@ -0,0 +1,30 @@
+package backend.manuhub.news;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record NewsGetResponse(
+        Long id,
+        String title,
+        String description,
+        String link,
+        String originalLink,
+        @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+        LocalDateTime publishedAt
+) {
+
+    public static NewsGetResponse from(News news) {
+        return NewsGetResponse.builder()
+                .id(news.getId())
+                .title(news.getTitle())
+                .description(news.getDescription())
+                .link(news.getLink())
+                .originalLink(news.getOriginalLink())
+                .publishedAt(news.getPublishedAt())
+                .build();
+    }
+}
