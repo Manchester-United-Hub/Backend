84cbaf7bb1689912f81e7f8291ea63427ffb1ef6
asdfzxcv53 <sskij@naver.com>
2026-05-11 22:29:36 +0900

    feat: add @EnableRetry configuration #7

--- src/main/java/backend/manuhub/ManuhubApplication.java
+++ src/main/java/backend/manuhub/ManuhubApplication.java
@@ -3,9 +3,11 @@ package backend.manuhub;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.retry.annotation.EnableRetry;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableRetry
 public class ManuhubApplication {
 
 	public static void main(String[] args) {
