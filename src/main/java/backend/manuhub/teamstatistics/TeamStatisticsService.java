ed7b3f1e8763d896dee4c059a7af4c15b4ca3273
asdfzxcv53 <sskij@naver.com>
2026-05-11 21:04:59 +0900

    refactor: remove save logic from TeamStatisticsService #7

--- src/main/java/backend/manuhub/teamstatistics/TeamStatisticsService.java
+++ src/main/java/backend/manuhub/teamstatistics/TeamStatisticsService.java
@@ -1,7 +1,6 @@
 package backend.manuhub.teamstatistics;
 
-import backend.manuhub.external.teamstatistics.TeamStatisticsApiResponse;
-import backend.manuhub.external.teamstatistics.TeamStatisticsClient;
+import backend.manuhub.teamstatistics.dto.TeamStatisticsResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,37 +13,13 @@ import java.util.List;
 public class TeamStatisticsService {
 
     private final TeamStatisticsRepository teamStatisticsRepository;
-    private final TeamStatisticsClient teamStatisticsClient;
 
     @Transactional(readOnly = true)
     public List<TeamStatisticsResponse> getAllTeamStatistics() {
-
-        log.info("[TeamStatistics] 전체 시즌 기록 조회");
-
         List<TeamStatistics> statisticsList = teamStatisticsRepository.findAllByOrderBySeasonAsc();
 
         return statisticsList.stream()
                 .map(TeamStatisticsResponse::from)
                 .toList();
-    }
-
-    @Transactional
-    public void saveTeamStatistics(Integer season) {
-        log.info("[TeamStatistics] {}년도 기록 저장 시작", season.toString());
-
-        if(teamStatisticsRepository.existsBySeason(season)) {
-            log.warn(">>> TeamStatistics --> EntityAlreadyExistsException. season = {}. TeamStatistics Entity Already Exists",
-                    season);
-            return;
-        }
-
-        TeamStatisticsApiResponse response =
-                teamStatisticsClient.getTeamStatistics(season);
-
-        TeamStatistics teamStatistics = response.toEntity();
-
-        teamStatisticsRepository.save(teamStatistics);
-
-        log.info("[TeamStatistics] {}년도 기록 저장 완료", season.toString());
     }
 }
