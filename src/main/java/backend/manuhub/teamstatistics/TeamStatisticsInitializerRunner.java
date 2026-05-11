4cb0cb445acd44c042f658d64c8efd2fc8ac852d
asdfzxcv53 <sskij@naver.com>
2026-05-11 22:12:38 +0900

    feat: add TeamStatisticsInitializer with CommandLineRunner #7

---
+++ src/main/java/backend/manuhub/teamstatistics/TeamStatisticsInitializerRunner.java
@@ -0,0 +1,41 @@
+package backend.manuhub.teamstatistics;
+
+import backend.manuhub.exception.ApiInvalidResponseException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TeamStatisticsInitializerRunner implements CommandLineRunner {
+
+    private final TeamStatisticsInitializeService teamStatisticsInitializeService;
+
+    @Override
+    public void run(String... args){
+        int lastSeason = getLastSeason();
+
+        for(int i = lastSeason - 2; i <= lastSeason; i++){
+            try{
+                teamStatisticsInitializeService.saveTeamStatistics(i);
+
+            } catch (ApiInvalidResponseException e){
+                log.error(">>> TeamStatisticsInitializerRunner --> ApiInvalidResponseException, season : {}", i, e);
+            }
+        }
+    }
+ 
+    public int getLastSeason() {
+        LocalDate now = LocalDate.now();
+        LocalDate deadline = LocalDate.of(now.getYear(), 6, 1);
+
+        if (now.isAfter(deadline)) {
+            return now.getYear() - 1;
+        }
+        return now.getYear() - 2;
+    }
+}
