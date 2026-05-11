77fdebb00ad095fdedfb1d55abaf1baf66925993
asdfzxcv53 <sskij@naver.com>
2026-05-11 22:24:30 +0900

    refactor: improve TeamStatisticsClient response handling #7

--- src/main/java/backend/manuhub/external/teamstatistics/TeamStatisticsClient.java
+++ src/main/java/backend/manuhub/external/teamstatistics/TeamStatisticsClient.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Optional;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
@@ -18,38 +20,36 @@ public class TeamStatisticsClient {
     private final RestClient restClient;
     private final static Long teamId = 33L;
 
-    public TeamStatisticsApiResponse getTeamStatistics(Integer season) {
+    public Optional<TeamStatisticsApiResponse> getTeamStatistics(Integer season) {
 
-        String uri = UriComponentsBuilder
-                .fromPath("/standings")
-                .queryParam("league", 39)
-                .queryParam("team", teamId)
-                .queryParam("season", season)
-                .toUriString();
+        log.info("[API-Football] 팀 통계 요청 season={}", season);
 
-        log.info("[API-Football] 요청 시작 uri={}, season={}",
-                uri, season);
+        Optional<TeamStatisticsApiResponse> result = Optional.ofNullable(
+                        restClient.get()
+                                .uri(uriBuilder -> uriBuilder
+                                        .path("/standings")
+                                        .queryParam("league", 39)
+                                        .queryParam("team", teamId)
+                                        .queryParam("season", season)
+                                        .build())
+                                .retrieve()
+                                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
+                                    throw new ApiClientException(ErrorCode.API_FOOTBALL_CLIENT_ERROR);
+                                })
+                                .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
+                                    throw new ApiServerException(ErrorCode.API_FOOTBALL_SERVER_ERROR);
+                                })
+                                .body(TeamStatisticsApiResponse.class)
+                )
+                .filter(res -> !res.response().isEmpty());
 
-        TeamStatisticsApiResponse response = restClient.get()
-                .uri(uri)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
-                    throw new ApiClientException(ErrorCode.API_FOOTBALL_CLIENT_ERROR);
-                })
-                .onStatus(HttpStatusCode::is5xxServerError, (req, res) -> {
-                    throw new ApiServerException(ErrorCode.API_FOOTBALL_SERVER_ERROR);
-                })
-                .body(TeamStatisticsApiResponse.class);
-
-        if (response == null || response.getResponse().isEmpty()) {
-            log.warn("[API-Football] 응답 없음 season={}",
-                    season);
-            return null;
+        if (result.isEmpty()) {
+            log.warn(">>> TeamStatisticsClient --> Exception. API response is empty. season={}", season);
+            throw new ApiServerException(ErrorCode.API_FOOTBALL_SERVER_ERROR);
+        } else {
+            log.info("[API-Football] 팀 통계 요청 성공 season={}", season);
         }
 
-        log.info("[API-Football] 요청 성공 season={}",
-                season);
-
-        return response;
+        return result;
     }
 }
